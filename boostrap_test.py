7029d8055a3c74655ce97ca0fc12d0589eea02e2
rabbit <luzhiyong@AIR.local>
2020-10-13 18:01:41 +0800

    add_boost_test

--- boostrap_test.py
+++ boostrap_test.py
@@ -1,33 +1,33 @@
 # -*- coding:UTF-8 -*-
 from fastapi import FastAPI, Form
 from starlette.requests import Request
-from fastapi import FastAPI, Form, File, UploadFile
 from starlette.templating import Jinja2Templates
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
+
 @app.post("/user/")
 async def files(
     request: Request,
-    username:str = Form(...),
-    password:str = Form(...),
+    username: str = Form(...),
+    password: str = Form(...),
     ):
     print("username", username)
     print("password", password)
-    return remplates.TemplateResponse(
-    'boostrap_index.html',
-    {
-        'request': request,
-        'username': username,
+    return templates.TemplateResponse(
+        'boostrap_index.html',
+        {
+            'request': request,
+            'username': username,
+        })
 
-    } )
 
 @app.get("/")
-async def main(request: Request)
-    return templates.TemplateResponse('signin.html', {"request": request})
-
+async def main(request: Request):
+    return templates.TemplateResponse('boostrap_signin.html', {"request": request})
 
 
 if __name__ == "__main__":
