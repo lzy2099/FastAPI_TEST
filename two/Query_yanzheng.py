2879dc01ed292e3dc1b218821df2c3e759d3ad24
rabbit <luzhiyong@AIR.local>
2020-10-20 17:10:48 +0800

    qq

--- two/Query_yanzheng.py
+++ two/Query_yanzheng.py
@@ -7,18 +7,23 @@ app = FastAPI()
 
 # 限制长度
 @app.get("/items_limit/")
-async def gets_items(q:str = Query(None, min_length=2, max_length=10)):
-    results = {"items": [{'item_id': "puppy", "item_id": "kitty"}]}
+async def gets_items(q: str = Query(None, min_length=2, max_length=10)):  # 如果将 None 改为 ... 则此项是必填项
+    results = {"items": [{'item_id': "puppy"}, {"item_id": "kitty"}]}
     if q:
         results.update({"q": q})
     return results
 
 
-# 正则
+# 正则匹配
 @app.get("/items_regex/")
-async def gets_tems2(q:str=Query(None,min_length=2,max_length=20,regex="^A")):
-    results = {"items": [{"item_id": "appale"},{"item_id":"orange"}]}
+async def gets_tems2(q: str = Query(None, min_length=2, max_length=20, regex="^A")):  # 要求参数必须要字母A开头
+    results = {"items": [{"item_id": "appale"}, {"item_id": "orange"}]}
+    if q:
+        results.update({"q": q})
+    return results
+
 
 if __name__ == "__main__":
     import uvicorn
+
     uvicorn.run(app, host="127.0.0.1", port=8000)
